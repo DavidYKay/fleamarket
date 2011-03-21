a021d2bc65d5bb22a035f42f498c728156056e73
David Young-Chan Kay <davidykay@gmail.com>
2011-03-21 17:34:24 -0400

    Basic edit item functionality in place.

--- fleamarket/listings/urls.py
+++ fleamarket/listings/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 #from django.views.generic import list_detail, create_update
 from listings.models import Item
 import views
+from forms import NewItemForm
 
 info_dict = {
     'queryset': Item.objects.all(),
@@ -25,6 +26,13 @@ urlpatterns = patterns('',
     (r'^new/$', views.new),
 
     (r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
+    (r'^(?P<object_id>\d+)/edit$', 'django.views.generic.create_update.update_object', 
+        {
+            'form_class': NewItemForm,
+            #'model': Item,
+        }
+    ),
+
 
     # We're naming this 'item_results'
     url(r'^(?P<object_id>\d+)/results/$', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='items/results.html'), 'item_results'),
